2543a50eb38565c3bcef0523963fcccbdb5cf7b8
ArtScienceMK <2026kulakov.am@student.letovo.ru>
2026-07-06 15:31:58 +0300

    check

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -11,7 +11,7 @@ Graph::Graph()
 vector<string> Graph::correct(const string & s) {
     vector<string> words;
     string cur = "";
-    for (auto c : s) {
+    for (char c : s) {
         if (!is_alpha(c) && !is_digit(c)) {
             words.push_back(cur);
             cur = "";
@@ -22,9 +22,33 @@ vector<string> Graph::correct(const string & s) {
     if ((int)cur.size() > 0) {
         words.push_back(cur);
     }
-    for (string words : words) {
+    vector<string> correct_words;
+    for (string cur : words) {
         int curMatches = 0;
         int bestMatches = 0;
-        string
+        string best = "";
+        bool stopped = false;
+        for (string word : allWords_) {
+            curMatches = 0;
+            if (word.size() != cur.size()) continue;
+            for (int i = 0; i < (int)word.size(); i++) {
+                if (cur[i] == word[i]) {
+                    curMatches++;
+                }
+            }    
+            if (curMatches == (int)cur.size()) {
+                stopped = true;
+                correct_words.push_back(cur);
+                break;
+            }
+            if (curMatches > bestMatches) {
+                bestMatches = curMatches;
+                best = cur;
+            }
+        }
+        if (!stopped) {
+            correct_words.push_back(cur);
+        }
     }
+    return correct_words;
 }
