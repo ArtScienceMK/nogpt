c7d7c69ecd62b51ea7ed90021cb7e99997f1b8c0
Mr-Soul-Forest <stepan.sniff@gmail.com>
2026-07-06 15:37:34 +0300

    words

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <vector>
 
-Graph::Graph() : m_graph(0)
+Graph::Graph() : m_graph(0),
+                 m_words(0)
 {
 }
 
@@ -10,44 +11,65 @@ string Graph::findCurrentWord(const string &s)
 {
     int need = (int)s.size();
     string cur = "";
-    for (char c : s) {
-        if (!is_alpha(c) && !is_digit(c)) {
+    for (char c : s)
+    {
+        if (!is_alpha(c) && !is_digit(c))
+        {
             words.push_back(cur);
             cur = "";
-        } else {
+        }
+        else
+        {
             cur += c;
         }
     }
-    if ((int)cur.size() > 0) {
+    if ((int)cur.size() > 0)
+    {
         words.push_back(cur);
     }
     vector<string> correct_words;
-    for (string cur : words) {
+    for (string cur : words)
+    {
         int curMatches = 0;
         int bestMatches = 0;
         string best = "";
         bool stopped = false;
-        for (string word : allWords_) {
+        for (string word : allWords_)
+        {
             curMatches = 0;
-            if (word.size() != cur.size()) continue;
-            for (int i = 0; i < (int)word.size(); i++) {
-                if (cur[i] == word[i]) {
+            if (word.size() != cur.size())
+                continue;
+            for (int i = 0; i < (int)word.size(); i++)
+            {
+                if (cur[i] == word[i])
+                {
                     curMatches++;
                 }
-            }    
-            if (curMatches == (int)cur.size()) {
+            }
+            if (curMatches == (int)cur.size())
+            {
                 stopped = true;
                 correct_words.push_back(cur);
                 break;
             }
-            if (curMatches > bestMatches) {
+            if (curMatches > bestMatches)
+            {
                 bestMatches = curMatches;
                 best = cur;
             }
         }
-        if (!stopped) {
+        if (!stopped)
+        {
             correct_words.push_back(cur);
         }
     }
     return correct_words;
+}
+
+void loadFromFile(string &filename)
+{
+    ifstream file(filename);
+
+    int V;
+    file >> V;
 }
--- src/Graph.hpp
+++ src/Graph.hpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
 class Graph
 {
     vector<vector<pair<int, float>>> m_graph;
+    vector<string> m_words;
     string findCurrentWord(const string &s);
 
 public:
     Graph();
+    void loadFromFile(string &filename);
 };
